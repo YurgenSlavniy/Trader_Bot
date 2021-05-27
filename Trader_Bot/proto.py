b8b6addf4f2e014cdfcf404d78d26855f99c620b
Andrey Iv <andrey.iv.cherkessk@gmail.com>
2021-05-27 22:16:11 +0300

    Изменения во всем файле proto.py

--- Trader_Bot/proto.py
+++ Trader_Bot/proto.py
@@ -1,20 +1,13 @@
 from re import match
 """
-* необходимо обеспечить проверку вводимых данных
-
-Если использовать программу для себя, то известно какие данные нужно ввести.
-	если вместо RUB/EXM будет введено [rub/exm], [123/456], [100]
-	также пара добавляется в список pirs, а что если она вобще некоректна
-	даже если ее формат с виду коректен BLA/MUDOFON
-	Например: Минимальная и максимальная длина len(pains[i]) + регистер символов
-
+* подумать о вводимых данных дополнительно
 * необходимо улучшить выводимые сообщения, чтобы было коротко и понятно
+* необходимо вычислить повторяющиеся участки кода
+* необходимо создать библиотеку в отдельном файле
 """
 
-# start ----------------- ЗДЕСЬ БЫЛИ ИЗМЕНЕНИЯ ------------------------ #
-
 ############################## LIBRARY ##################################
-# function для проверки корректности пары
+# function проверка корректности пары
 def isvalid_pair(pair):
 	# если длина корректна length True
 	length = len(pair) > 6 and len(pair) < 20
@@ -25,6 +18,24 @@ def isvalid_pair(pair):
 		return True
 	else:
 		return False
+# futnction установить статус пользователя 
+def detect_status(balance):
+	status = ''
+	if balance < 11111:
+		status = 'babyplay'
+	
+	elif balance >= 11111 and balance < 55555:
+		status = 'junior'
+	
+	elif balance >= 55555 and balance < 111111:
+		status = 'Segnior'
+	
+	elif balance >= 111111 and balance < 1111111:
+		status = 'VipSegnior'
+	
+	elif balance >= 1111111:
+		status = 'TraderKing'
+	return status
 ############################# END LIBRARY ###############################
 
 # Список пар
@@ -60,21 +71,25 @@ message = """
 # Выводим сообщение
 print(message)
 
-# Вывод имеющихся пар
+""" 
+Вывод имеющихся пар
+pair:<10s означает выровнять EXM/RUB : по < правому краю с шириной поля 10 символов
+"""
 for i in range( len(pairs) ):
 	print(" {pair:<10s} ".format(pair=pairs[i]), end='')	
 	if (i + 1) % 4 == 0:
 		print()
 
 """
-+ Ввод названя пары, вводить можно только такой формат EXM/RUB и exm/rub
++ Ввод названия пары, вводить можно только такой формат EXM/RUB и exm/rub
 + удалить пробелы strip
 + переводим в вегхний регистер upper
++ если пользователь нажал интер не вводя ничего, присваиваем FAIL/IN' неудачный ввод
 """
-user_info['pair_name'] = input('\n\n Пара: ').strip().upper()
+user_info['pair_name'] = input('\n\n Пара: ').strip().upper() or 'fail/in'
 
 # Если пара валидная в ней нет некорректных символов
-if isvalid_pair(user_info['pair_name']):
+if isvalid_pair(user_info['pair_name']) and user_info['pair_name'] != 'fail/in':
 	# Если пары нет в списке pairs, то добавляем
 	if user_info['pair_name'] not in pairs:
 		pairs.append(user_info['pair_name'])
@@ -87,59 +102,69 @@ break_pair = user_info['pair_name'].split('/')
 user_info['buy_currency'] = break_pair[0] # вылюта которую покупаем 
 user_info['use_currency'] = break_pair[1] # вылюта которую используем для покупки
 
-# end ----------------- ДАЛЬШЕ НЕТ ИЗМЕНЕНИЙ ------------------------ #
-print(pairs)
-print(user_info)
-input('---------------------------------------------------------------')
+
 # Формируем сообщение
 message = """
  ЗАПРОС: 2
- Сколько вы готовы инвестировать в эту валютную пару в рублях?
-"""
+ Сколько вы готовы инвестировать в эту валютную пару в {currency}?
+""".format(currency=user_info['use_currency'])
+
 print(message)
 
 # Ввод суммы
-user_info['balance'] = int(input(' Сумма: '))
+user_info['balance'] = int(input(' Сумма: ') or 0)
+
+# Если баланс равен нулю
+if user_info['balance'] == 0:
+	raise Exception('Сумма должна быть больше ' + str(user_info['balance']))
 
 # Определяем статус пользователя
-if user_info['balance'] < 11111:
-	user_info['status'] = 'babyplay'
+user_info['status'] = detect_status(user_info['balance'])
 
-elif user_info['balance'] >= 11111 and user_info['balance'] < 55555:
-	user_info['status'] = 'junior'
-
-elif user_info['balance'] >= 55555 and user_info['balance'] < 111111:
-	user_info['status'] = 'Segnior'
-
-elif user_info['balance'] >= 111111 and user_info['balance'] < 1111111:
-	user_info['status'] = 'VipSegnior'
-
-elif user_info['balance'] >= 1111111:
-	user_info['status'] = 'TraderKing'
-
-# Формируем сообщение
+""" 
++ Формируем сообщение
++ в тексте будет найден шаблон {balance}
++ balance=user_info['balance'] будет подставленно значение по имени аргумента
+"""
 message = """
- Ваш баланс: {balance} рублей.
+ Ваш баланс: {balance} {currency}.
 
  ЗАПРОС: 3
  Тип операции: Покупка введите BUY  или buy
                Продажа введите SELL или sell
-""".format(balance=user_info['balance'])
+""".format(
+	balance=user_info['balance'],
+	currency=user_info['use_currency']
+	)
 
 # Вывод баланса и предложение на дальнейшие действия
 print(message)
 
 # Ввод действия buy или sell + удалить пробелы
-user_info['action'] = input(' Действие: ').strip()
+action = input(' Действие: ').strip().lower() or 'fail/in'
+
+# если выбрана корректная опрерация
+if action == 'sell' or action == 'buy' and action != 'fail/in':
+	user_info['action'] = action
+else:
+	raise Exception('Действие не корректно ' + action)
+
+user_info['action'] = action
 
 print('\n ЗАПРОС: 4\n Введите биржевую цену ', end='')
 
 # Ввод текущей цены buy или sell
-if user_info['action'].lower() == 'buy':
-	user_info['current_price'] = float(input('"ПОКУПКИ" на данный момент: '))
+if user_info['action'] == 'buy':
+	current_price = float(input('"ПОКУПКИ" на данный момент: ') or 0.0)
 
-if user_info['action'].lower() == 'sell':
-	user_info['current_price'] = float(input('"ПРОДАЖИ" на данный момен: '))
+if user_info['action'] == 'sell':
+	current_price = float(input('"ПРОДАЖИ" на данный момен: ') or 0.0)
+
+
+if current_price == 0:
+	raise Exception('Цена должна быть больше ' + str(current_price))
+
+user_info['current_price'] = current_price
 
 # Формируем сообщение
 message = """
@@ -147,16 +172,18 @@ message = """
   Вы хотите торговать в валютной паре - {pair}
   Вы готовы инвестировать в неё       - {balance} рублей
   Ваш статус                          - {status}
-  Тип производимой операции           - {action} по цене "{current_price}"
 
  Подождите...
 """.format(
 		pair=user_info['pair_name'],
-		status=user_info['status'], 
+		status=user_info['status'],
 		balance=user_info['balance'],
-		action=user_info['action'],
-		current_price=user_info['current_price']
 	)
 
 # Вывод введенной информации
 print(message)
+
+for el in user_info:
+	print(el, ':', user_info[el])
+
+# Кода становится слишком много
