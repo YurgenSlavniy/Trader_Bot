d19af5ec3f5b59655ebc9d013b22719908bd06bf
YurgenSlavniy <108W108@gmail.com>
2021-05-31 14:07:13 +0300

    add change to comments in example. py

--- Trader_Bot/example.py
+++ Trader_Bot/example.py
@@ -5,8 +5,8 @@ currency      = 'EXM'
 use_currency  = 'RUB'
 status        = 'babyplay'
 action        = 'buy'
-balance       = 456
-current_price = 0.69
+balance       = 100
+current_price = 1
 
 
  # действия без длинных описаний...
@@ -50,19 +50,32 @@ balance = balance - fixorderprice
 # Выставляем третий ордер:
 current_price = current_price - pricestep
 print('3) BUY   цена: ', current_price,  ' ', use_currency , 'сумма ордера: ', fixorderprice, ' ', use_currency, 'количество покупаемой валюты: ', fixorderprice / current_price, ' ', currency )
+balance = balance - fixorderprice
 # Выставляем четвёртый ордер:
 current_price = current_price - pricestep
 print('4) BUY   цена: ', current_price,  ' ', use_currency , 'сумма ордера: ', fixorderprice, ' ', use_currency, 'количество покупаемой валюты: ', fixorderprice / current_price, ' ', currency )
+balance = balance - fixorderprice
 # Выставляем слдующий ордер:
 current_price = current_price - pricestep
 print('5) BUY   цена: ', current_price,  ' ', use_currency , 'сумма ордера: ', fixorderprice, ' ', use_currency, 'количество покупаемой валюты: ', fixorderprice / current_price, ' ', currency )
+balance = balance - fixorderprice
+current_price = current_price - pricestep
 print(...)
 # И т.д пока у нас не закончится баланс balance = 0 вышли из цикла - ордера расставлены!
 
 # когда выводим пользователю получаем :
-# *) BUY   цена:  0.9919504065040651  RUB, сумма ордера:  10.0  RUB, количество покупаемой валюты:  10.081149152650728  EXM
+# * например
 # цена:  0.9919504065040651  , количество покупаемой валюты:  10.081149152650728  EXM можно округлять до 4 знаков после запятой...
+# а лучше округлять по некоторому принципу:
+# если цена - current_price >= 1  , округляем до 2 ух знаков после запятой.
+# если цена - current_price < 1  , округляем до 4 ух знаков после запятой.
+# use_currency - округляем до 2 ух знаков после запятой.
+# fixorderprice / current_price округляем до 2 ух знаков после запятой.
+
 
 # я тестировал работу с другими данными и забыл, что в папке репозитория работаю.
 # и пока не создам коммит, не могу переключиться на другую ветку в гит кракене
 # немного изменил вывод с использованием переменных currency, use_currency
+
+# запихнуть это в красивый цикл и всё прекрасно работает!
+# меняю дынные, отлично всё просчитывает!
