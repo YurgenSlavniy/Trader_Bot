4abccc706691c61a12663a175dfa2778b7761bdc
Andrey Iv <andrey.iv.cherkessk@gmail.com>
2021-05-31 09:43:36 +0300

    Add vars to example.py

---
+++ Trader_Bot/example.py
@@ -0,0 +1,11 @@
+
+
+ pair_name     = 'EXM/RUB'
+ currency      = 'EXM'
+ use_currency  = 'RUB'
+ status        = 'babyplay'
+ action        = 'buy'
+ balance       = 1234
+ current_price = 1
+
+ # действия без длинных описаний...
